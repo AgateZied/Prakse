7f7d81f2f89441c7bdc86d7a2a3403f7fb4088fc
AgateZied <63899066+AgateZied@users.noreply.github.com>
2021-09-02 14:32:27 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
+import pypyodbc
+
+conn = pypyodbc.connect('Driver={ODBC Driver 17 for SQL Server};'
+  'Server=localhost;'
+  'Database=TestDB;'
+  'uid=sa;pwd=AgateZiedina2*') 
+
+
+
 def main():
   print("Hello World!")
 
