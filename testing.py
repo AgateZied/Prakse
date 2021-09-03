32b300ad2682ce272ebd3f156c84ae1145f36c3c
AgateZied <63899066+AgateZied@users.noreply.github.com>
2021-09-03 13:38:05 +0300

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 import pypyodbc
-
+import logging
+def logFileWriting(txt):
+  logging.basicConfig(filename="someLogFile.log",
+    format='%(asctime)s %(message)s',
+    filemode='a')
+  logger=logging.getLogger()
+  logger.setLevel(logging.DEBUG)
+  logger.info(txt)
 def connections():
   conn = None
   conn = pypyodbc.connect('Driver={ODBC Driver 17 for SQL Server};'
@@ -9,6 +16,10 @@ def connections():
     'uid=sa;pwd=AgateZiedina2*')
 
   return conn
+
+logingInformation = 'Today is friday!'
+logFileWriting(logingInformation)
+
 def create_table(connectionBool):
   cursor = connectionBool.cursor()
   cursor.execute('''CREATE TABLE testTable(
@@ -16,18 +27,24 @@ def create_table(connectionBool):
       LastName  TEXT NOT NULL );''')
   connectionBool.commit()
  # connectionBool.close()
+
 def insert_table(connectionBool):
   cursor = connectionBool.cursor()
   cursor.execute('''INSERT INTO testTable(FirstName,LastName) VALUES
-      ('Santa','Santina'),
+      ('Santaa','Santina'),
       ('Janis', 'Kalnins')''')
   connectionBool.commit()
   connectionBool.close()
+
 def main():
   print("Hello!")
   connBool =connections()
-  create_table(connBool)
-  insert_table(connBool)
+  #create_table(connBool)
+  #insert_table(connBool)
+
+  if connBool:
+    connBool.close()
+    logFileWriting('Logegd out')
   print("BYE!")
 
 if __name__ == "__main__":
