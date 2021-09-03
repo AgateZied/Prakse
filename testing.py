4fc4653490b6f222c836a7b8de4e54098943d015
AgateZied <63899066+AgateZied@users.noreply.github.com>
2021-09-03 11:48:57 +0300

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,22 +1,33 @@
 #!/usr/bin/env python3
 import pypyodbc
 
-def conn():
+def connections():
+  conn = None
   conn = pypyodbc.connect('Driver={ODBC Driver 17 for SQL Server};'
     'Server=localhost;'
     'Database=TestDB;'
     'uid=sa;pwd=AgateZiedina2*')
-  cursor = conn.cursor()
-  cursor.execute('''INSERT INTO testTable(FirstName,LastName) VALUES
-    ('Agate','Ziedina'),
-    ('Juris', 'Kalnins')''')
-  conn.commit()
-  conn.close()
-  return conn
 
+  return conn
+def create_table(connectionBool):
+  cursor = connectionBool.cursor()
+  cursor.execute('''CREATE TABLE testTable(
+      FirstName TEXT NOT NULL,
+      LastName  TEXT NOT NULL );''')
+  connectionBool.commit()
+ # connectionBool.close()
+def insert_table(connectionBool):
+  cursor = connectionBool.cursor()
+  cursor.execute('''INSERT INTO testTable(FirstName,LastName) VALUES
+      ('Santa','Santina'),
+      ('Janis', 'Kalnins')''')
+  connectionBool.commit()
+  connectionBool.close()
 def main():
   print("Hello!")
-  conn()
+  connBool =connections()
+  create_table(connBool)
+  insert_table(connBool)
   print("BYE!")
 
 if __name__ == "__main__":
